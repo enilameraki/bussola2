48b14cf4e89b433ac7a5eea3c65a6651ce1598c1
enilameraki <estudiodecriacaomeraki@gmail.com>
2026-03-16 15:30:29 -0300

    vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
+  base: '/bussola2/', // Nome do seu repositório entre barras
   plugins: [react()],
 })
