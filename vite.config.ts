b01570c0e0737ee771e9e15fdd9ecdfda8d3404b
enilameraki <estudiodecriacaomeraki@gmail.com>
2026-03-16 15:54:02 -0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,5 @@
 import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react' // Verifique se é esse o nome no seu package.json
+import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
